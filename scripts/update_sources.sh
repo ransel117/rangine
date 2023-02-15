bf21b8a0a4e02ffc6db3324fc0582aa18bff5d14
Ransel117 <jonatanronnberg@gmail.com>
2023-02-16 00:33:19 +0100

    actually updated submodule

--- scripts/update_sources.sh
+++ scripts/update_sources.sh
@@ -5,7 +5,7 @@ EXTERNAL="$SOURCE/external"
 
 echo "----- Get latest version of ranmath -----"
 cd "$EXTERNAL/ranmath"
-git pull
+git pull origin main
 cd $SOURCE
 
 command -v glad >/dev/null 2>&1 || { echo >&2 "I require glad but it's not installed.\nAborting..."; exit 1; }
@@ -15,3 +15,6 @@ cd "$EXTERNAL"
 rm -r glad
 glad --api gl:core=3.3 --extensions "" --out-path glad c
 cd $SOURCE
+
+echo "----------- Update submodules -----------"
+git submodule update --remote --merge
